295251e39470061f8b5fd763d3a14a0913de690e
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2026-04-03 23:07:00 +0530

    comments section added to blog page and home page

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ const Blog = require('../models/blog');
 const multer = require('multer');
 const path = require('path');
 const router = Router();
+const Comment = require('../models/comment');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -35,10 +36,22 @@ router.post("/", upload.single("coverImage"), async (req, res) => {
 
 router.get("/:id", async (req, res) => {
     const blog = await Blog.findById(req.params.id).populate('createdBy');
+    const comments = await Comment.find({ blogId: req.params.id }).populate('createdBy');
     return res.render("blog", {
         user: req.user,
-        blog
+        blog,
+        comments
     });
 });
+
+router.post('/comment/:blogId', async (req, res) => {
+    await Comment.create({
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id
+    });
+    return res.redirect(`/blog/${req.params.blogId}`);
+});
+
 
 module.exports = router;
